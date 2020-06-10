cde88c9938d5bf37756928fcd6f77f83d48dac8e
XJC <853696509@qq.com>
2020-06-10 20:34:12 +0800

    添加创建型模式之单例模式 类加载方式(静态内部类)

--- DesignPattern/DesignPattern_01_创建型模式/src/main/java/_01_单例模式/SingletonPattern.java
+++ DesignPattern/DesignPattern_01_创建型模式/src/main/java/_01_单例模式/SingletonPattern.java
@@ -138,6 +138,28 @@ class LazyStyle_DoubleCheck{
     }
 }
 
+//静态内部类
+/**
+ 优缺点说明:
+ |-这种方式采用了类装载的机制来保证初始化实例时只有一个线程
+ |-静态内部类方式在Singleton类被装载时并不会立即实例化，而是在需要实例化时，调用getInstance()方法，才会装载到SingleInstance类，从而完成Singleton的实例化。
+ |-类的静态属性只会在第一次加载类的时候初始化，所以在这里，JVM帮组我们保证了线程的安全性，在类进行初始化时，别的线程时无法进入的。
+ |-优点:避免线程不安全，利用静态内部类的特点实现延迟加载，效率高。
+ 结论：推荐使用
+ **/
+class SingletonPattern_InnerStaticClass {
+    //构造器私有化
+    private SingletonPattern_InnerStaticClass(){}
+    //提供一个静态内部类，该类中有一个静态属性instance
+    private static class SingletonInstance {
+        private final static SingletonPattern_InnerStaticClass INSTANCE = new SingletonPattern_InnerStaticClass();
+    }
+    //提供一个静态方法，直接返回SigletonInstance.INSTANCE
+    public static SingletonPattern_InnerStaticClass getInstance(){
+        return SingletonInstance.INSTANCE;
+    }
+}
+
 public class SingletonPattern {
     public static void main(String[] args) {
         //饿汉式--->静态变量
@@ -181,5 +203,12 @@ public class SingletonPattern {
         System.out.println(instance11.hashCode()); //1173230247
         System.out.println(instance12.hashCode()); //1173230247
         System.out.println(instance11 == instance12); //true
+
+        //类加载--->静态内部类
+        SingletonPattern_InnerStaticClass instance13 = SingletonPattern_InnerStaticClass.getInstance();
+        SingletonPattern_InnerStaticClass instance14 = SingletonPattern_InnerStaticClass.getInstance();
+        System.out.println(instance13.hashCode()); //856419764
+        System.out.println(instance14.hashCode()); //856419764
+        System.out.println(instance13 == instance14); //true
     }
 }

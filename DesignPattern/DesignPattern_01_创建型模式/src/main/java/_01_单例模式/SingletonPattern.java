72d93bd20c5a84c7d021b240fcf0af4bf25408dc
XJC <853696509@qq.com>
2020-06-10 18:20:19 +0800

    添加创建型模式之单例模式 饿汉式(静态代码块)方法

--- DesignPattern/DesignPattern_01_创建型模式/src/main/java/_01_单例模式/SingletonPattern.java
+++ DesignPattern/DesignPattern_01_创建型模式/src/main/java/_01_单例模式/SingletonPattern.java
@@ -10,10 +10,26 @@ package _01_单例模式;
 class HungryStyle_StaticVariable{
     //构造器私有化
     private HungryStyle_StaticVariable(){}
-    //创建内部对象实例
+    //内部创建对象实例
     private final static HungryStyle_StaticVariable instance = new HungryStyle_StaticVariable();
     //获取对象实例的公共接口
     public static HungryStyle_StaticVariable getInstance(){
+        return instance;
+    }
+}
+
+//饿汉式--->静态代码块
+class HungryStyle_StaticBlock{
+    //构造器私有化
+    private HungryStyle_StaticBlock(){}
+    //创建对象
+    private static HungryStyle_StaticBlock instance;
+    //代码块创建对象实例
+    static {
+        instance = new HungryStyle_StaticBlock();
+    }
+    //获取对象实例的公共接口
+    public static HungryStyle_StaticBlock getInstance(){
         return instance;
     }
 }
@@ -26,5 +42,12 @@ public class SingletonPattern {
         System.out.println(instance1.hashCode()); //1735600054
         System.out.println(instance2.hashCode()); //1735600054
         System.out.println(instance1 == instance2); //true
+
+        //饿汉式--->静态代码块
+        HungryStyle_StaticBlock instance3 = HungryStyle_StaticBlock.getInstance();
+        HungryStyle_StaticBlock instance4 = HungryStyle_StaticBlock.getInstance();
+        System.out.println(instance3.hashCode()); //21685669
+        System.out.println(instance4.hashCode()); //21685669
+        System.out.println(instance3 == instance4); //true
     }
 }
